d4cf8c8713e9fbcd039a4014cb5acdd29dca49b7
admin <admin>
2019-10-14 17:26:22 +0600

    поиск по названию и описанию задачи

--- todo_list/webapp/views/project_view.py
+++ todo_list/webapp/views/project_view.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.urls import reverse
+from django.utils.http import urlencode
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from webapp.forms import ProjectForm
+from webapp.forms import ProjectForm, SimpleSearchForm
 from webapp.models import Project
 
 
@@ -12,8 +14,32 @@ class ProjectIndexView(ListView):
     # paginate_by = 3
     # paginate_orphans = 1
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
     def get_queryset(self):
-        return Project.objects.all()
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(name__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProjectDetailView(DetailView):
