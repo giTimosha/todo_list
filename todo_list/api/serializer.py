e46dce0bcabd22d9de639294503f92c18a5b8ac0
admin <admin>
2019-12-05 19:06:07 +0600

    done

--- todo_list/api/serializer.py
+++ todo_list/api/serializer.py
@@ -1,5 +1,4 @@
-from rest_framework import serializers
-
+from rest_framework import serializers, viewsets
 
 from webapp.models import Project, Task
 
@@ -8,3 +7,11 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'name', 'description', 'created_at', 'updated_at')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            'id', 'description', 'full_description', 'status',
+            'type', 'date', 'project')
--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from api.serializer import ProjectSerializer
-from webapp.models import Project
+from api.serializer import ProjectSerializer, TaskSerializer
+from webapp.models import Project, Task
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
